e00ae843a2831813643ed5cc03f54d1e1f029080
Spike Sprague <rasprague@gmail.com>
2019-11-07 10:11:42 -0800

    add some opz tools to midi_tool.py, WIP

--- tools/midi_tool.py
+++ tools/midi_tool.py
@@ -43,7 +43,14 @@ def midiInPort():
 
 
 def midiCallback(msg):
-    if msg.type is not 'clock':
+    if msg.type is 'clock':
+        return
+    elif msg.type is 'sysex':
+        h = []
+        for b in msg.data:
+            h.append(hex(b))
+        print('sysex [%s]' % ' '.join(h))
+    else:
         print(msg)
 
 def setupMidiPorts():
@@ -51,8 +58,10 @@ def setupMidiPorts():
     # g['liveOutPort'] = mido.open_output(launchpad_ports[0])
     # g['standaloneInPort'] = mido.open_input(launchpad_ports[1], callback=midiCallback)
     # g['standaloneOutPort'] = mido.open_output(launchpad_ports[1])
-    g['midiInPort'] = mido.open_input(launchpad_ports[2], callback=midiCallback)
-    g['midiOutPort'] = mido.open_output(launchpad_ports[2])
+    #g['midiInPort'] = mido.open_input(launchpad_ports[2], callback=midiCallback)
+    #g['midiOutPort'] = mido.open_output(launchpad_ports[2])
+    g['opzInPort'] = mido.open_input('OP-Z MIDI 1', callback=midiCallback)
+    g['opzOutPort'] = mido.open_output('OP-Z MIDI 1')
 
 setupMidiPorts()
 
@@ -60,8 +69,15 @@ setupMidiPorts()
 def octatrackCCDumpRequest():
     return mido.Message('control_change', channel=10, control=61, value=0)
 
-def ccDump():
+def octatrackCCDump():
     midiOutPort().send(octatrackCCDumpRequest())
 ####
+def opzStatusRequest():
+    # msg = mido.Message('sysex', data=[0x00, 0x20, 0x76, 0x01, 0x00, 0x03, 0x2D, 0x0E, 0x05])
+    msg = mido.Message('sysex', data=[0x00, 0x20, 0x76, 0x01, 0x00, 0x01, 0x4E, 0x2E, 0x06])
+    g['opzOutPort'].send(msg)
+
+####
+
 import code
 code.interact(local=locals())
